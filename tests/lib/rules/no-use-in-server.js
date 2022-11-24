3944e9dacad5dc7c3de89774eac944e329ca948a
shaobeichen <shaobeichen@outlook.com>
2022-11-24 19:15:14 +0800

    chore: add template

--- tests/lib/rules/no-use-in-server.js
+++ tests/lib/rules/no-use-in-server.js
@@ -2,30 +2,93 @@
  * @fileoverview Do not use client variables on the server
  * @author shaobeichen
  */
-"use strict";
+'use strict'
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-const rule = require("../../../lib/rules/no-use-in-server"),
-  RuleTester = require("eslint").RuleTester;
+const rule = require('../../../lib/rules/no-use-in-server')
 
+const RuleTester = require('eslint').RuleTester
+
+const parserOptions = {
+  ecmaVersion: 2018,
+  sourceType: 'module',
+}
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
-ruleTester.run("no-use-in-server", rule, {
+const ruleTester = new RuleTester()
+ruleTester.run('no-use-in-server', rule, {
   valid: [
     // give me some code that won't trigger a warning
+    // {
+    //   filename: 'test.vue',
+    //   code: `
+    //       export default {
+    //         created() {
+    //           const path = this.$route.path
+    //         },
+    //         beforeCreate() {
+    //           const path = this.$route.params.foo
+    //         }
+    //       }
+    //     `,
+    //   parserOptions,
+    // },
   ],
 
   invalid: [
     {
-      code: "document.querySelecter('#id')",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      filename: 'test.vue',
+      code: `
+          export default {
+            created() {
+              const path = window.location.pathname
+            },
+            beforeCreate() {
+              const foo = document.foo
+            }
+          }
+        `,
+      errors: [
+        {
+          message: 'Unexpected window in created.',
+          type: 'MemberExpression',
+        },
+        {
+          message: 'Unexpected document in beforeCreate.',
+          type: 'MemberExpression',
+        },
+      ],
+      parserOptions,
     },
+    // {
+    //   filename: 'test.vue',
+    //   code: `
+    //       export default {
+    //         created() {
+    //           document.foo = 'bar'
+    //         },
+    //         beforeCreate() {
+    //           window.foo = 'bar'
+    //         }
+    //       }
+    //     `,
+    //   errors: [
+    //     {
+    //       message: 'Unexpected document in created.',
+    //       type: 'MemberExpression',
+    //     },
+    //     {
+    //       message: 'Unexpected window in beforeCreate.',
+    //       type: 'MemberExpression',
+    //     },
+    //   ],
+    //   parserOptions,
+    // },
   ],
-});
+})
